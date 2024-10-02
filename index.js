741e3a66987013753e39ee2aa9f73d8e779c18ed
riyarane46 <154919910+riyarane46@users.noreply.github.com>
2024-10-02 23:05:56 +0530

    added the redirecting feature on the main page

--- index.js
+++ index.js
@@ -19,4 +19,13 @@ window.addEventListener('scroll', function(){
  header.classList.toggle("sticky", window.scrollY>0);
 })
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
